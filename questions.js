7be5b73cf90940e55c9eace56f59536685d7b2d4
Jr0ckER <papipayo65@gmail.com>
2026-07-22 21:28:47 +0900

    Update questions.js

--- questions.js
+++ questions.js
@@ -1,73 +1,143 @@
 const questions = [
 
 {
-    question:"Saucy Dogのボーカルは？",
-    choices:["石原慎也","野田洋次郎","尾崎世界観","藤原基央"],
-    answer:0,
-    reward:20
+    question: "Saucy Dogのボーカルは？",
+    choices: ["石原慎也", "野田洋次郎", "尾崎世界観", "藤原基央"],
+    answer: 0,
+    reward: 20
 },
 
 {
-    question:"SHISHAMOの出身地は？",
-    choices:["神奈川県","大阪府","東京都","北海道"],
-    answer:0,
-    reward:20
+    question: "SHISHAMOの出身地は？",
+    choices: ["神奈川県", "大阪府", "東京都", "北海道"],
+    answer: 0,
+    reward: 20
 },
 
 {
-    question:"RADWIMPSのボーカルは？",
-    choices:["野田洋次郎","石原慎也","川谷絵音","後藤正文"],
-    answer:0,
-    reward:20
+    question: "RADWIMPSのボーカルは？",
+    choices: ["野田洋次郎", "川谷絵音", "尾崎世界観", "後藤正文"],
+    answer: 0,
+    reward: 20
 },
 
 {
-    question:"BUMP OF CHICKENのボーカルは？",
-    choices:["藤原基央","野田洋次郎","山内総一郎","尾崎世界観"],
-    answer:0,
-    reward:20
+    question: "BUMP OF CHICKENのボーカルは？",
+    choices: ["藤原基央", "野田洋次郎", "尾崎世界観", "川上洋平"],
+    answer: 0,
+    reward: 20
 },
 
 {
-    question:"ヨルシカのコンポーザーは？",
-    choices:["n-buna","ACAね","川谷絵音","TK"],
-    answer:0,
-    reward:20
+    question: "ヨルシカのコンポーザーは？",
+    choices: ["n-buna", "ACAね", "TK", "川谷絵音"],
+    answer: 0,
+    reward: 20
 },
 
 {
-    question:"ずっと真夜中でいいのに。のボーカル名義は？",
-    choices:["ACAね","suis","ikura","milet"],
-    answer:0,
-    reward:20
+    question: "ずっと真夜中でいいのに。のボーカル名義は？",
+    choices: ["ACAね", "suis", "ikura", "milet"],
+    answer: 0,
+    reward: 20
 },
 
 {
-    question:"My Hair is Badの出身地は？",
-    choices:["新潟県","徳島県","大阪府","東京都"],
-    answer:0,
-    reward:20
+    question: "My Hair is Badの出身地は？",
+    choices: ["新潟県", "東京都", "大阪府", "徳島県"],
+    answer: 0,
+    reward: 20
 },
 
 {
-    question:"マカロニえんぴつのボーカルは？",
-    choices:["はっとり","石原慎也","尾崎世界観","北村匠海"],
-    answer:0,
-    reward:20
+    question: "The PADDLESの出身地は？",
+    choices: ["大阪府", "東京都", "福岡県", "北海道"],
+    answer: 0,
+    reward: 20
 },
 
 {
-    question:"クリープハイプのボーカルは？",
-    choices:["尾崎世界観","川谷絵音","野田洋次郎","藤原基央"],
-    answer:0,
-    reward:20
+    question: "クリープハイプのボーカルは？",
+    choices: ["尾崎世界観", "石原慎也", "野田洋次郎", "藤原基央"],
+    answer: 0,
+    reward: 20
 },
 
 {
-    question:"ASIAN KUNG-FU GENERATIONのボーカルは？",
-    choices:["後藤正文","川上洋平","細美武士","金井政人"],
-    answer:0,
-    reward:20
+    question: "マカロニえんぴつのボーカルは？",
+    choices: ["はっとり", "尾崎世界観", "山内総一郎", "川谷絵音"],
+    answer: 0,
+    reward: 20
+},
+
+{
+    question: "ASIAN KUNG-FU GENERATIONのボーカルは？",
+    choices: ["後藤正文", "川上洋平", "細美武士", "野田洋次郎"],
+    answer: 0,
+    reward: 20
+},
+
+{
+    question: "BLUE ENCOUNTのボーカルは？",
+    choices: ["田邊駿一", "山中拓也", "谷口鮪", "尾崎世界観"],
+    answer: 0,
+    reward: 20
+},
+
+{
+    question: "UNISON SQUARE GARDENのボーカルは？",
+    choices: ["斎藤宏介", "藤原基央", "野田洋次郎", "はっとり"],
+    answer: 0,
+    reward: 20
+},
+
+{
+    question: "KANA-BOONのボーカルは？",
+    choices: ["谷口鮪", "尾崎世界観", "石原慎也", "山内総一郎"],
+    answer: 0,
+    reward: 20
+},
+
+{
+    question: "フレデリックの代表曲は？",
+    choices: ["オドループ", "花束", "君と羊と青", "シュガーソングとビターステップ"],
+    answer: 0,
+    reward: 20
+},
+
+{
+    question: "Mrs. GREEN APPLEのボーカルは？",
+    choices: ["大森元貴", "藤原基央", "野田洋次郎", "川谷絵音"],
+    answer: 0,
+    reward: 20
+},
+
+{
+    question: "go!go!vanillasのボーカルは？",
+    choices: ["牧達弥", "尾崎世界観", "谷口鮪", "石原慎也"],
+    answer: 0,
+    reward: 20
+},
+
+{
+    question: "04 Limited Sazabysのボーカルは？",
+    choices: ["GEN", "はっとり", "尾崎世界観", "山中拓也"],
+    answer: 0,
+    reward: 20
+},
+
+{
+    question: "indigo la Endのボーカルは？",
+    choices: ["川谷絵音", "野田洋次郎", "藤原基央", "石原慎也"],
+    answer: 0,
+    reward: 20
+},
+
+{
+    question: "Official髭男dismのボーカルは？",
+    choices: ["藤原聡", "藤原基央", "尾崎世界観", "谷口鮪"],
+    answer: 0,
+    reward: 20
 }
 
 ];
